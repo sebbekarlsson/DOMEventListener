9462b4d6930c6ac59c6783a81dd862143d7a2a2b
Sebastian karlsson <sebbekarlsson97@gmail.com>
2017-12-20 12:18:15 +0100

    enabling user defined read and write functions

--- README.md
+++ README.md
@@ -28,3 +28,33 @@
 > something like:
 
     'changed', <unified diff between the new data and the old>
+
+### Changing where data is stored
+> By default, the element is stored in the `RAM` memory, you can change this  
+> by setting read and write functions.
+
+> For example, if you want to use _MongoDB_:
+
+    from domeventlistener.listen import Listener
+    from myapp.mongo import db
+    
+    
+    def read_element():
+        return db.collections.find_one({'name': 'element'})
+
+    def write_element(element_str):
+        return db.collections.update_one(
+            {'name': 'element', 'element_str': element_str},
+            upsert=True
+        )
+    
+    ...
+
+    listener = Listener(
+        'https://stackoverflow.com/questions',
+        '#mainbar',
+        event_handler,
+        read_element=read_element,
+        write_element=write_element
+    )
+    ...
--- domeventlistener/listen.py
+++ domeventlistener/listen.py
@@ -9,12 +9,33 @@ EVENT_CHANGED = 'changed'
 
 class Listener(object):
 
-    def __init__(self, domain, query, event_handler):
+    def __init__(
+        self,
+        domain,
+        query,
+        event_handler,
+        read_element=None,
+        write_element=None
+    ):
         self.domain = domain
         self.query = query
         self.event_handler = event_handler
+        self.read_element = read_element
+        self.write_element = write_element
         self.session = Session()
-        self.element = None
+        self.element_str = None
+
+    def read_element_str(self):
+        if not self.read_element:
+            return str(self.element_str)
+
+        return self.read_element
+
+    def write_element_str(self, element_str):
+        if not self.write_element:
+            self.element_str = str(element_str)
+        else:
+            return self.write_element(element_str)
 
     def on_event(self, event_type, data):
         return self.event_handler(event_type, data)
@@ -25,12 +46,11 @@ class Listener(object):
             document = BeautifulSoup(resp.text, 'html.parser')
             _element = document.select(self.query)
 
-            if str(_element) != str(self.element):
+            if str(_element) != self.read_element_str():
                 self.on_event(
                     EVENT_CHANGED,
-                    unidiff_output(str(self.element), str(_element))
+                    unidiff_output(self.read_element_str(), str(_element))
                 )
 
-            self.element = _element
-
+            self.write_element_str(str(_element))
             time.sleep(sleep_time)
