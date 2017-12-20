90aaa4bdafef621a3bb3a7507a1b0bdbca3eaa26
Sebastian karlsson <sebbekarlsson97@gmail.com>
2017-12-20 11:07:19 +0100

    added description

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from setuptools import setup
 setup(
     name='DOMEventListener',
     version='1.0',
+    description='Subscripe on DOM elements, trigger methods when they change',
     install_requires=[
         'requests',
         'bs4'
