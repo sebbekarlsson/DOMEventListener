d98ccb8f03b121478123d048187015c457cd4526
Sebastian karlsson <sebbekarlsson97@gmail.com>
2017-12-21 10:06:15 +0100

    version upgrade

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='DOMEventListener',
-    version='1.7',
+    version='1.8',
     description='Subscripe on DOM elements, trigger methods when they change',
     url='https://github.com/sebbekarlsson/DOMEventListener',
     install_requires=[
